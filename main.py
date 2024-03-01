75b8b2cb8394067ac6d19b60b097741a716fe376
amri souhaiel <amrisouhail96@gmail.com>
2024-03-01 21:16:38 +0100

    main

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
 def main():
-    
-
-
-
-    # Test de la classe Calculs
-    tableau = [10, 20, 30, 40, 50]
-    print("\nSomme:", Calculs.calculerSomme(tableau))
-    print("Moyenne:", Calculs.calculerMoyenne(tableau))
-
-
-
-
-if __name__ == "__main__":
-    main()
