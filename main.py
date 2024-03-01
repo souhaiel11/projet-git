cd039f0b236e74b67537e8ac440c80e6f38571e5
wissemhafyenne <wissemhafyenne09@gmail.com>
2024-03-01 21:33:11 +0100

    ajouter la fichier wissem et ajouter les modification de main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from souhaiel import Calculs;
-
+from wissem import GestionChaine;
 
 def main():
     ma_liste = ["a", "b", "c"]
@@ -8,6 +8,14 @@ def main():
     calculs_instance.afficher_liste()
     calculs_instance.modifier_liste(0, "3")
     calculs_instance.afficher_liste()
+
+    chaine = "Bonjour, monde!"
+
+    chaine_majuscule = GestionChaine.convertir_en_majuscule(chaine)
+    print("Chaîne en majuscule :", chaine_majuscule)
+
+    longueur_chaine = GestionChaine.calculer_longueur_chaine(chaine)
+    print("Longueur de la chaîne :", longueur_chaine)
 
 if __name__ == "__main__":
     main()
---
+++ wissem.py
@@ -0,0 +1,7 @@
+class GestionChaine:
+
+    def convertir_en_majuscule(chaine):
+        return chaine.upper()
+
+    def calculer_longueur_chaine(chaine):
+        return len(chaine)
